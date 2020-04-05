8c8760413c1d994a55efc2cee40d56288c87c90a
Lucas <lucas.hirao@outlook.com>
2020-04-05 13:27:08 -0300

    Capitulo 1

---
+++ Capítulo 1/jogo_adivinhar_numero.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Jogo de adivinhar o numero - Use a cabeca
+puts 'Jogo de adivinhar número'
+puts 'Qual o seu nome? '
+name = gets.chomp
+puts "Bem vindo, #{name}!"
+tentatives = 0
+correct_answer = false
+answer = rand(1..100)
+until tentatives == 10 || correct_answer
+  puts "Tentiva #{tentatives + 1} de 10."
+  puts 'Adivinhe um número de 1 a 100: '
+  number = gets.to_i
+  tentatives += 1
+  if number == answer
+    puts 'Parabéns, você acertou!'
+    correct_answer = true
+  elsif number < answer
+    puts 'Respota errada. A resposta é maior que o número escolhido'
+  else
+    puts 'Respota errada. A resposta é menor que o número escolhido'
+  end
+end
+
+puts "Número de tentativas esgotado. A respota era #{answer}" unless answer
